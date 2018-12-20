78b2094153e4e157d363cc01f420a54494b42a35
alynch <andrewlynch10@gmail.com>
2018-12-21 05:15:02 +0700

    Switch.

--- src/Main2.kt
+++ src/Main2.kt
@@ -16,8 +16,8 @@ object loader {
 
 //        HikariCP.default("jdbc:h2:/Users/alynch/git/atlassian/moreglo/my-akka-http-project/db", "sa", "")
 //        HikariCP.default("jdbc:h2:/home/ec2-user/search/sandbox/db", "sa", "")
-//        HikariCP.default("jdbc:hsqldb:file:/home/ec2-user/search/sandbox/hsqldb", "sa", "")
-        HikariCP.default("jdbc:hsqldb:file:/Users/alynch/git/atlassian/moreglo/my-akka-http-project/hsqldb", "sa", "")
+        HikariCP.default("jdbc:hsqldb:file:/home/ec2-user/search/sandbox/hsqldb", "sa", "")
+//        HikariCP.default("jdbc:hsqldb:file:/Users/alynch/git/atlassian/moreglo/my-akka-http-project/hsqldb", "sa", "")
         SessionImpl.defaultDataSource = { HikariCP.dataSource() }
 
     }
